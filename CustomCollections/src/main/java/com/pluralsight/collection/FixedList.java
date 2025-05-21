dd2f7cbe5f42f7406efa46a8fb1d70ac4c9beb64
Jrobinson718 <jrobinson6284@gmail.com>
2025-05-21 09:11:12 -0400

    Feat: Using generic listing to make my code more malleable as generic lists can use referebce types and wrapper classes

--- CustomCollections/src/main/java/com/pluralsight/collection/FixedList.java
+++ CustomCollections/src/main/java/com/pluralsight/collection/FixedList.java
@@ -1,4 +1,24 @@
 package com.pluralsight.collection;
 
-public class FixedList {
+import java.util.ArrayList;
+import java.util.List;
+
+public class FixedList<T> {
+    List<T> items = new ArrayList<>();
+    int maxSize;
+
+
+    public FixedList(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public List<T> getItems() {
+        return items;
+    }
+
+    public void add(T item) {
+        if (this.items.size() < this.maxSize) {
+            items.add(item);
+        }
+    }
 }
